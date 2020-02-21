6adbfab73177865697c818b3ce627abaee3ca32c
Nathaly Riaño Bejarano <natharb09@gmail.com>
2020-02-21 16:23:59 -0500

    feat: getting the selected category and filter shows by category

--- js/index.js
+++ js/index.js
@@ -3,16 +3,20 @@ import { shows } from '../mock-api.js';
 const $category = document.getElementById('category');
 let selectedCategory = '';
 
-export function onChangeCategory() {
+function filterShowsByCategory(category) {
+  let showsByCategory = shows.filter(show => show.genres.includes(category));
+  console.log('showsByCategory:::: ', showsByCategory);
+}
+
+function onChangeCategory(e) {
   selectedCategory = $category.options[$category.selectedIndex].value;
   console.log('selectedCategory:::: ', selectedCategory);
+  filterShowsByCategory(selectedCategory);
 }
+
+$category.addEventListener('change', e => onChangeCategory(e));
 
 (function load() {
   console.log('$category:::: ', $category);
-  console.log('shows:::: ', shows);
-  let categories = new Set();
-  shows.forEach(show => show.genres.forEach(genre => categories.add(genre)));
-  categories = [...(new Set(categories))].sort();
-  console.log('categories:::: ', categories);
+  onChangeCategory();
 })();
